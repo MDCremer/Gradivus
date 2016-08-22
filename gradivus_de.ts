0607ad9a8053554d6a78a9fe050da4fdb8db438e
Marcus Daniel Cremer <mdc@gradivus.de>
2016-08-22 18:08:14 +0200

    Übersetzung aktualisiert

--- gradivus_de.ts
+++ gradivus_de.ts
@@ -8,5 +8,73 @@
         <source>Gradivo (www.gradivus.de)</source>
         <translation>Gradivus (www.gradivus.de)</translation>
     </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="46"/>
+        <source>Informo</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="82"/>
+        <source>Agordoj</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="100"/>
+        <source>Datumoj apogilo</source>
+        <translation>Datensicherung</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="116"/>
+        <source>Reprezentadoj</source>
+        <translation>Darstellungen</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="129"/>
+        <source>Kartoj redaktilo</source>
+        <translation>Spielfeldbearbeitung</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="142"/>
+        <source>Milita historia vortaro</source>
+        <translation>Militärhistorisches Wörterbuch</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="155"/>
+        <source>Vido</source>
+        <translation>Ansicht</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="186"/>
+        <source>muta</source>
+        <translation>stumm</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="208"/>
+        <source>Mapo</source>
+        <translation>Karte</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="244"/>
+        <source>Page 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="257"/>
+        <source>Page 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>datumojApogilo</name>
+    <message>
+        <location filename="datumojapogilo.ui" line="17"/>
+        <source>Datumoj apogilo</source>
+        <translation>Datensicherung</translation>
+    </message>
+    <message>
+        <location filename="datumojapogilo.ui" line="32"/>
+        <source>Abortu</source>
+        <translation>Abbrechen</translation>
+    </message>
 </context>
 </TS>
