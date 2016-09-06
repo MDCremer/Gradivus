b984833155e2b367d00ac27fbb5afa1951f33868
Marcus Daniel Cremer <mdc@gradivus.de>
2016-09-06 15:57:37 +0200

    Dialog zur Konfiguration entworfen

--- konfiguro.cpp
+++ konfiguro.cpp
@@ -1,6 +1,18 @@
+#include <QList>
+#include <QLocale>
+#include <QStringList>
 #include "konfiguro.h"
 #include "ui_konfiguro.h"
 
 konfiguro::konfiguro(QWidget *gepatro):QDialog(gepatro),ui(new Ui::konfiguro)
 {ui->setupUi(this);
+ QList<QLocale> cxiujKulturoj=QLocale::matchingLocales(QLocale::AnyLanguage,QLocale::AnyScript,QLocale::AnyCountry);
+ QStringList lingvoKodoj;
+ for(int indekso=0;indekso<cxiujKulturoj.length();++indekso)
+  lingvoKodoj.append(cxiujKulturoj[indekso].name());
+ lingvoKodoj.sort();
+ ui->kulturo->addItem(tr("loka\u0135aro"));
+ for(int indekso=0;indekso<lingvoKodoj.length();++indekso)
+  if(ui->kulturo->findText(lingvoKodoj[indekso])==-1)
+   ui->kulturo->addItem(lingvoKodoj[indekso]);
 }
