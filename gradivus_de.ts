315ac3b317e3b2e205a629f5fa4fa2d7f9d463d8
Marcus Daniel Cremer <mdc@gradivus.de>
2016-09-17 22:49:22 +0200

    Schriftgröße von neun auf zwölf Punkt gesetzt.

--- gradivus_de.ts
+++ gradivus_de.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de_DE">
+<TS version="2.1" language="de_DE">
 <context>
     <name>QObject</name>
     <message>
@@ -32,136 +32,145 @@
 <context>
     <name>cxefaFenestro</name>
     <message>
-        <location filename="cxefafenestro.ui" line="14"/>
+        <location filename="cxefafenestro.ui" line="19"/>
         <source>Gradivo (www.gradivus.de)</source>
         <translation>Gradivus (www.gradivus.de)</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="46"/>
+        <location filename="cxefafenestro.ui" line="51"/>
         <source>Informo</source>
         <translation>Information</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="82"/>
+        <location filename="cxefafenestro.ui" line="87"/>
         <source>Agordoj</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="100"/>
+        <location filename="cxefafenestro.ui" line="105"/>
         <source>Konfiguro</source>
         <translation>Konfiguration</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="107"/>
+        <location filename="cxefafenestro.ui" line="112"/>
         <source>Datumoj apogilo</source>
         <translation>Datensicherung</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="136"/>
+        <location filename="cxefafenestro.ui" line="141"/>
         <source>Reprezentadoj</source>
         <translation>Darstellungen</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="149"/>
+        <location filename="cxefafenestro.ui" line="154"/>
         <source>Kartoj redaktilo</source>
         <translation>Spielfeldbearbeitung</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="162"/>
-        <source>Milita historia vortaro</source>
-        <translation>Militärhistorisches Wörterbuch</translation>
+        <location filename="cxefafenestro.ui" line="167"/>
+        <source>Aludo Laboro</source>
+        <translation>Nachschlagewerk</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="175"/>
-        <source>Vido</source>
-        <translation>Ansicht</translation>
+        <location filename="cxefafenestro.ui" line="173"/>
+        <source>Bibliografio</source>
+        <translation>Bibliographie</translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="206"/>
-        <source>muta</source>
-        <translation>stumm</translation>
-    </message>
-    <message>
-        <location filename="cxefafenestro.ui" line="228"/>
-        <source>Mapo</source>
-        <translation>Karte</translation>
-    </message>
-    <message>
-        <location filename="cxefafenestro.ui" line="264"/>
+        <location filename="cxefafenestro.ui" line="291"/>
         <source>Page 1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="cxefafenestro.ui" line="277"/>
+        <location filename="cxefafenestro.ui" line="304"/>
         <source>Page 2</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Milita historia vortaro</source>
+        <translation type="vanished">Militärhistorisches Wörterbuch</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="202"/>
+        <source>Vido</source>
+        <translation>Ansicht</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="233"/>
+        <source>muta</source>
+        <translation>stumm</translation>
+    </message>
+    <message>
+        <location filename="cxefafenestro.ui" line="255"/>
+        <source>Mapo</source>
+        <translation>Karte</translation>
     </message>
 </context>
 <context>
     <name>datumojApogilo</name>
     <message>
-        <location filename="datumojapogilo.ui" line="17"/>
+        <location filename="datumojapogilo.ui" line="22"/>
         <source>Datumoj apogilo</source>
         <translation>Datensicherung</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="71"/>
+        <location filename="datumojapogilo.ui" line="76"/>
         <source>Limigoj</source>
         <translation>Einschränkungen</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="89"/>
+        <location filename="datumojapogilo.ui" line="94"/>
         <source>nur savu bildoj kai sonoj tiel kiel referencoj al ĝi kiam malkaŝo estas permesita</source>
         <translation>nur Bilder und Geräusche sichern, bei denen die Weitergabe erlaubt ist</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="101"/>
+        <location filename="datumojapogilo.ui" line="106"/>
         <source>subskribo devas inkluzivi:</source>
         <translation>Signatur muß enthalten:</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="119"/>
+        <location filename="datumojapogilo.ui" line="124"/>
         <source>nova ol:</source>
         <translation>neuer als:</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="138"/>
+        <location filename="datumojapogilo.ui" line="143"/>
         <source>Datumtipoj</source>
         <translation>Datenarten</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="165"/>
+        <location filename="datumojapogilo.ui" line="170"/>
         <source>Agordoj</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="172"/>
+        <location filename="datumojapogilo.ui" line="177"/>
         <source>Lingvoj</source>
         <translation>Sprachen</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="158"/>
+        <location filename="datumojapogilo.ui" line="163"/>
         <source>Identigiloj</source>
         <translation>Bezeichnungen</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="179"/>
+        <location filename="datumojapogilo.ui" line="184"/>
         <source>Literaturoj</source>
         <translation>Quellenangaben</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="190"/>
+        <location filename="datumojapogilo.ui" line="195"/>
         <source>Elektu ĉiuj</source>
         <translation>Alle auswählen</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="221"/>
+        <location filename="datumojapogilo.ui" line="226"/>
         <location filename="datumojapogilo_prieksportu.cpp" line="24"/>
         <source>Abortu</source>
         <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="datumojapogilo.ui" line="244"/>
+        <location filename="datumojapogilo.ui" line="249"/>
         <source>Eksportu</source>
         <translation>Exportieren</translation>
     </message>
@@ -231,47 +240,47 @@
 <context>
     <name>konfiguro</name>
     <message>
-        <location filename="konfiguro.ui" line="14"/>
+        <location filename="konfiguro.ui" line="19"/>
         <source>Konfiguro</source>
         <translation>Konfiguration</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="62"/>
+        <location filename="konfiguro.ui" line="67"/>
         <source>Subskribon</source>
         <translation>Signatur</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="80"/>
+        <location filename="konfiguro.ui" line="85"/>
         <source>Inicialoj:</source>
         <translation>Namenskürzel:</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="93"/>
+        <location filename="konfiguro.ui" line="98"/>
         <source>Lingvoj</source>
         <translation>Sprachen</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="113"/>
+        <location filename="konfiguro.ui" line="118"/>
         <source>Kulturo:</source>
         <translation>Kultur:</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="147"/>
+        <location filename="konfiguro.ui" line="152"/>
         <source>↟</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="162"/>
+        <location filename="konfiguro.ui" line="167"/>
         <source>↑</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="177"/>
+        <location filename="konfiguro.ui" line="182"/>
         <source>↓</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="192"/>
+        <location filename="konfiguro.ui" line="197"/>
         <source>↡</source>
         <translation></translation>
     </message>
@@ -294,17 +303,17 @@
         <translation>Es gibt keine Änderungen!</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="232"/>
+        <location filename="konfiguro.ui" line="237"/>
         <source>Malfaru</source>
         <translation>Rückgängig</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="239"/>
+        <location filename="konfiguro.ui" line="244"/>
         <source>Konfirmi</source>
         <translation>Bestätigen</translation>
     </message>
     <message>
-        <location filename="konfiguro.ui" line="212"/>
+        <location filename="konfiguro.ui" line="217"/>
         <source>Lasu</source>
         <translation>Verlassen</translation>
     </message>
@@ -316,11 +325,11 @@
     </message>
     <message>
         <source>Eraro [011]!</source>
-        <translation>Fehler [011]!</translation>
+        <translation type="vanished">Fehler [011]!</translation>
     </message>
     <message>
         <source>Eraro [010]!</source>
-        <translation>Fehler [010]!</translation>
+        <translation type="vanished">Fehler [010]!</translation>
     </message>
     <message>
         <location filename="konfiguro_prikonfirmu.cpp" line="29"/>
@@ -329,7 +338,7 @@
     </message>
     <message>
         <source>Ŝarĝoj estas stokitaj.</source>
-        <translation>Änderungen werden gespeichert.</translation>
+        <translation type="vanished">Änderungen werden gespeichert.</translation>
     </message>
     <message>
         <location filename="konfiguro_prikonfirmu.cpp" line="87"/>
@@ -339,16 +348,16 @@
     <message>
         <source>Ekzistas neniu ŝarĝo!</source>
         <oldsource>Ekzistas neniu Ŝarĝo!</oldsource>
-        <translation>Es gibt keine Änderungen!</translation>
+        <translation type="vanished">Es gibt keine Änderungen!</translation>
     </message>
     <message>
         <source>Ekzistas neniu sxangxo!</source>
-        <translation>Änderungen sind nicht vorhanden!</translation>
+        <translation type="vanished">Änderungen sind nicht vorhanden!</translation>
     </message>
     <message numerus="yes">
         <source>%n ŝarĝoj estas savitaj!</source>
         <oldsource>%1 ŝarĝoj estas savitaj!</oldsource>
-        <translation>
+        <translation type="vanished">
             <numerusform>%n Änderung wurden gespeichert!</numerusform>
             <numerusform>%n Änderungen wurden gespeichert!</numerusform>
         </translation>
@@ -356,17 +365,25 @@
     <message>
         <source>%1 u015danu011doj estas konservitaj!</source>
         <oldsource>%1 ŝanĝoj estas konservitaj!</oldsource>
-        <translation>%1 Änderungen wurden gesichert!</translation>
+        <translation type="vanished">%1 Änderungen wurden gesichert!</translation>
     </message>
     <message>
         <source>Ekzistas neniu korekto!</source>
         <oldsource>Ekzistas neniu ŝanĝo!</oldsource>
-        <translation>Änderungen sind nicht vorhanden!</translation>
+        <translation type="vanished">Änderungen sind nicht vorhanden!</translation>
     </message>
     <message>
         <location filename="konfiguro_primalfaru.cpp" line="19"/>
         <source>Ŝanĝoj malfaritaj!</source>
         <translation>Änderungen rückgängig gemacht!</translation>
+    </message>
+</context>
+<context>
+    <name>referencoj</name>
+    <message>
+        <location filename="referencoj.ui" line="19"/>
+        <source>Bibliografio</source>
+        <translation>Bibliographie</translation>
     </message>
 </context>
 </TS>
