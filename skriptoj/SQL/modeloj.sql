3fda141f805075002f12fe34ed7033276826a374
Marcus Daniel Cremer <mdc@gradivus.de>
2017-02-05 19:58:21 +0100

    Progreso kaj korektoj.

---
+++ skriptoj/SQL/modeloj.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS modeloj
+(uuid CHAR(22) NOT NULL,
+ jaro INT NOT NULL,
+ pezo REAL NOT NULL,
+ prezo REAL NOT NULL,
+ prezofluktuo INT NOT NULL,
+ utilavivo INT NOT NULL,
+ utilavivofluktuo INT NOT NULL,
+ fontoj CHAR(12) NOT NULL,
+ subskribo TEXT NOT NULL,
+ stato INTEGER NOT NULL,
+ PRIMARY KEY(uuid,jaro)
+);
